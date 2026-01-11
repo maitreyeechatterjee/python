928ac324ba0aeae2ccd4042a64c15f6e098e79e6
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-11 22:53:47 +0530

    Add solution for Sorted Copy

---
+++ sorted-copy/solution.py
@@ -0,0 +1,29 @@
+"""
+Problem: Sorted Copy
+URL: https://neetcode.io/problems/python-sorted-copy/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+from typing import Listfrom typing import List
+
+
+def sort_words(words: List[str]) -> List[str]:def sort_words(words: List[str]) -> List[str]:
+    pass    pass
+
+
+def sort_numbers(numbers: List[int]) -> List[int]:def sort_numbers(numbers: List[int]) -> List[int]:
+    pass    pass
+
+
+# do not modify below this line# do not modify below this line
+original_words = ["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]original_words = ["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]
+
+print(original_words)print(original_words)
+print(sort_words(original_words))print(sort_words(original_words))
+
+original_numbers = [1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]original_numbers = [1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]
+
+print(original_numbers)print(original_numbers)
+print(sort_numbers(original_numbers))print(sort_numbers(original_numbers))
