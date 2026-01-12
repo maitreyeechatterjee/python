e55b090a5c99b3a39312bbc935bdba8e4524d82d
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-12 19:57:11 +0530

    Update solution for Sorted Copy

--- sorted-copy/solution.py
+++ sorted-copy/solution.py
@@ -6,26 +6,7 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-from typing import Listfrom typing import List
-
-
-def sort_words(words: List[str]) -> List[str]:def sort_words(words: List[str]) -> List[str]:
-    sorted_words = sorted(words)    sorted_words = sorted(words)
-
 
 def sort_numbers(numbers: List[int]) -> List[int]:def sort_numbers(numbers: List[int]) -> List[int]:
-    sorted_numbers = sorted(numbers, reverse=True)    sorted_numbers = sorted(numbers, reverse=True)
-
-
-# do not modify below this line# do not modify below this line
-original_words = ["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]original_words = ["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]
-
-print(original_words)print(original_words)
-print(sort_words(original_words))print(sort_words(original_words))
-
-original_numbers = [1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]original_numbers = [1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]
-
-print(original_numbers)print(original_numbers)
-print(sort_numbers(original_numbers))print(sort_numbers(original_numbers))
-    return(sorted_words)    return(sorted_words)
     return(sorted_numbers)    return(sorted_numbers)
+
