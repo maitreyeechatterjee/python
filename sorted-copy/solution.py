be75a968d8e731e670b61b062127a092deb63418
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-12 19:57:26 +0530

    Update solution for Sorted Copy

--- sorted-copy/solution.py
+++ sorted-copy/solution.py
@@ -8,5 +8,11 @@ Solution by NeetCode GitHub Pusher
 
 
 def sort_numbers(numbers: List[int]) -> List[int]:def sort_numbers(numbers: List[int]) -> List[int]:
+    sorted_numbers = sorted(numbers, key=abs, reverse=True)    sorted_numbers = sorted(numbers, key=abs, reverse=True)
     return(sorted_numbers)    return(sorted_numbers)
 
+
+# do not modify below this line# do not modify below this line
+original_words = ["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]original_words = ["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]
+
+print(original_words)print(original_words)
