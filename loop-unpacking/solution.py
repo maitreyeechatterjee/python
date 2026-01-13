34b7b9c336b239cf5ab368fbc0485cab43cea183
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-13 20:49:18 +0530

    Update solution for Loop Unpacking

--- loop-unpacking/solution.py
+++ loop-unpacking/solution.py
@@ -9,7 +9,7 @@ Solution by NeetCode GitHub Pusher
         if score > best_score:        if score > best_score:
     for name, score in scores:    for name, score in scores:
     best_name, best_score = "", -1    best_name, best_score = "", -1
-def best_student(scores: List str, int]]) -> str:def best_student(scores: List str, int]]) -> str:
+def best_student(scores: List str, int) -> str:def best_student(scores: List str, int) -> str:
 
 
 from typing import List, Tuplefrom typing import List, Tuple
