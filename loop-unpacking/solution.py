6f43a9cbe9c915aa0c20cc6ac7c8baedc6c702d5
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-13 20:47:59 +0530

    Update solution for Loop Unpacking

--- loop-unpacking/solution.py
+++ loop-unpacking/solution.py
@@ -6,16 +6,12 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-from typing import List, Tuplefrom typing import List, Tuple
-
-
 def best_student(scores: List[Tuple[str, int]]) -> str:def best_student(scores: List[Tuple[str, int]]) -> str:
-    name = max(scores, key=lambda student: student[1])    name = max(scores, key=lambda student: student[1])
+    name = max(scores, key=lambda)    name = max(scores, key=lambda)
 
 
+    return (name)    return (name)
 # do not modify below this line# do not modify below this line
 print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
 print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
 print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
-print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
-    return (name)    return (name)
