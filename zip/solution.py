48fdbf8d62f68458a13934694e88c77a40f25e0c
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-13 21:31:40 +0530

    Add solution for Zip

---
+++ zip/solution.py
@@ -0,0 +1,21 @@
+"""
+Problem: Zip
+URL: https://neetcode.io/problems/python-zip/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+from typing import List, Dictfrom typing import List, Dict
+
+
+def group_names_and_scores(names: List[str], scores: List[int]) -> Dict[str, int]:def group_names_and_scores(names: List[str], scores: List[int]) -> Dict[str, int]:
+
+# do not modify below this line# do not modify below this line
+print(group_names_and_scores(["Alice", "Bob", "Charlie"], [90, 80, 70]))print(group_names_and_scores(["Alice", "Bob", "Charlie"], [90, 80, 70]))
+print(group_names_and_scores(["Jane", "Carol", "Charlie"], [25, 100, 60]))print(group_names_and_scores(["Jane", "Carol", "Charlie"], [25, 100, 60]))
+print(group_names_and_scores(["Doug", "Bob", "Tommy"], [80, 90, 100]))print(group_names_and_scores(["Doug", "Bob", "Tommy"], [80, 90, 100]))
+    result = {}    result = {}
+    for name, score in zip(names, score):    for name, score in zip(names, score):
+        result[name] = score        result[name] = score
+    return result    return result
