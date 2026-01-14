04d37c3452f996f4e6114871a0d3e62dfa7bccd0
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-14 19:29:38 +0530

    Update solution for Inequality

--- inequality/solution.py
+++ inequality/solution.py
@@ -9,7 +9,7 @@ Solution by NeetCode GitHub Pusher
 
 def is_arr_valid(names: List[str], max_length: int) -> bool:def is_arr_valid(names: List[str], max_length: int) -> bool:
     if 0 < len(names) <= max_length:    if 0 < len(names) <= max_length:
-        return True        return True
+    return True    return True
     return False    return False
 # do not modify below this line# do not modify below this line
 print(is_arr_valid("Alice", "Bob", "Charlie"], 3))print(is_arr_valid("Alice", "Bob", "Charlie"], 3))
