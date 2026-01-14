2ab84e04e8ee1ab3ace390c2bcaca864dda9032e
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-14 19:40:21 +0530

    Update solution for Min Max Shortcut

--- min-max-shortcut/solution.py
+++ min-max-shortcut/solution.py
@@ -10,17 +10,6 @@ Solution by NeetCode GitHub Pusher
 
 
 # do not modify below this line# do not modify below this line
+    return max_diff    return max_diff
 print(disallow_negatives(-2))print(disallow_negatives(-2))
 print(disallow_negatives(-1))print(disallow_negatives(-1))
-print(disallow_negatives(0))print(disallow_negatives(0))
-print(disallow_negatives(1))print(disallow_negatives(1))
-print(disallow_negatives(2))print(disallow_negatives(2))
-
-        diff = nums[i] - nums[i-1]        diff = nums[i] - nums[i-1]
-        if diff > max_diff:        if diff > max_diff:
-            max_diff = diff            max_diff = diff
-            return max_diff            return max_diff
-print(max_difference([1, 2, 3, 4, 5, 6, 7, 8, 9]))print(max_difference([1, 2, 3, 4, 5, 6, 7, 8, 9]))
-print(max_difference([1, 2, 3, 4, 5, 6, 8, 9]))print(max_difference([1, 2, 3, 4, 5, 6, 8, 9]))
-print(max_difference([10, 1, 3, 7]))print(max_difference([10, 1, 3, 7]))
-print(max_difference([2, 4, 7, 5, 7, 8, 4, 2]))print(max_difference([2, 4, 7, 5, 7, 8, 4, 2]))
