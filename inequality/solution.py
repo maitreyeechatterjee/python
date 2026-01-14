8f50506ae1c6b54ab298038fc58ceea8afeb0c56
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-14 19:32:25 +0530

    Update solution for Inequality

--- inequality/solution.py
+++ inequality/solution.py
@@ -12,6 +12,6 @@ def is_arr_valid(names: List[str], max_length: int) -> bool:def is_arr_va
         return True        return True
     return False    return False
 # do not modify below this line# do not modify below this line
-print(is_arr_valid("Alice", "Bob", "Charlie"], 3))print(is_arr_valid("Alice", "Bob", "Charlie"], 3))
+print(is_arr_valid(["Alice", "Bob", "Charlie"], 3))print(is_arr_valid(["Alice", "Bob", "Charlie"], 3))
 print(is_arr_valid(["Alice", "Bob", "Charlie"], 2))print(is_arr_valid(["Alice", "Bob", "Charlie"], 2))
 print(is_arr_valid(["Alice", "Bob", "Charlie"], 0))print(is_arr_valid(["Alice", "Bob", "Charlie"], 0))
