34f41ec4d17dc8216828c75250b4b7786d5d487f
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-14 19:54:04 +0530

    Update solution for Resizable List Part 1

--- resizable-list-part-1/solution.py
+++ resizable-list-part-1/solution.py
@@ -6,27 +6,11 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-
-
-def append_elements(arr1: List[int], arr2: List[int]) -> List[int]:def append_elements(arr1: List[int], arr2: List[int]) -> List[int]:
-    append_elements.append([4,5,6])    append_elements.append([4,5,6])
-
-
-def pop_n(arr: List[int], n: int) -> List[int]:def pop_n(arr: List[int], n: int) -> List[int]:
-    pass    pass
-
-
+    arr.insert(index, element)    arr.insert(index, element)
 def insert_at(arr: List[int], index: int, element: int) -> List[int]:def insert_at(arr: List[int], index: int, element: int) -> List[int]:
-    pass    pass
 
 
-# do not modify below this line# do not modify below this line
-print(append_elements([1, 2, 3], [4, 5, 6]))print(append_elements([1, 2, 3], [4, 5, 6]))
-print(append_elements([4, 3], [4, 5, 3]))print(append_elements([4, 3], [4, 5, 3]))
+    return arr    return arr
+        arr.pop()        arr.pop()
+    return arr    return arr
 
-print(pop_n([1, 2, 3, 4, 5], 2))print(pop_n([1, 2, 3, 4, 5], 2))
-print(pop_n([1, 2, 3, 4, 5], 6))print(pop_n([1, 2, 3, 4, 5], 6))
-print(pop_n([1, 2, 3, 4, 5], 5))print(pop_n([1, 2, 3, 4, 5], 5))
-
-print(insert_at([1, 2, 3, 4, 5], 2, 6))print(insert_at([1, 2, 3, 4, 5], 2, 6))
-print(insert_at([1, 2, 3, 4], 6, 5))print(insert_at([1, 2, 3, 4], 6, 5))
