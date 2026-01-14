e64e8a294c4750b18ee3517cf77f24cdbe991f06
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-14 19:36:24 +0530

    Add solution for Min Max Shortcut

---
+++ min-max-shortcut/solution.py
@@ -0,0 +1,27 @@
+"""
+Problem: Min Max Shortcut
+URL: https://neetcode.io/problems/python-min-max-shortcut/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+    return num if num >= 0 else 0    return num if num >= 0 else 0
+
+
+def max_difference(nums: List[int]) -> int:def max_difference(nums: List[int]) -> int:
+    pass     pass 
+
+
+
+# do not modify below this line# do not modify below this line
+print(disallow_negatives(-2))print(disallow_negatives(-2))
+print(disallow_negatives(-1))print(disallow_negatives(-1))
+print(disallow_negatives(0))print(disallow_negatives(0))
+print(disallow_negatives(1))print(disallow_negatives(1))
+print(disallow_negatives(2))print(disallow_negatives(2))
+def disallow_negatives(num: int) -> int:def disallow_negatives(num: int) -> int:
+
+
+from typing import Listfrom typing import List
+        
