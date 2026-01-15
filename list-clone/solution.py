eb6fa948a167b541c6c6e8767f8e1342c6e7bcd9
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-15 21:03:28 +0530

    Add solution for List Clone

---
+++ list-clone/solution.py
@@ -0,0 +1,25 @@
+"""
+Problem: List Clone
+URL: https://neetcode.io/problems/python-list-clone/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+from typing import Listfrom typing import List
+
+
+def remove_element(arr: List[int], element: int) -> List[int]:def remove_element(arr: List[int], element: int) -> List[int]:
+    pass    pass
+
+
+
+# do not modify below this line# do not modify below this line
+arr = [1, 3, 5, 7, 9]arr = [1, 3, 5, 7, 9]
+
+print(remove_element(arr, 3))print(remove_element(arr, 3))
+print(arr)print(arr)
+print(remove_element(arr, 9))print(remove_element(arr, 9))
+print(arr)print(arr)
+print(remove_element(arr, 1))print(remove_element(arr, 1))
+print(arr)print(arr)
