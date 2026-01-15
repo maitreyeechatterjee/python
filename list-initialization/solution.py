7aecb837eda546028ba81d452c601240c82c3764
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-15 19:54:54 +0530

    Add solution for List Initialization

---
+++ list-initialization/solution.py
@@ -0,0 +1,21 @@
+"""
+Problem: List Initialization
+URL: https://neetcode.io/problems/python-list-initialization/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+from typing import Listfrom typing import List
+
+
+def create_list_with_value(size: int, index: int, value: int) -> List[int]:def create_list_with_value(size: int, index: int, value: int) -> List[int]:
+    pass    pass
+
+
+
+# do not modify below this line# do not modify below this line
+print(create_list_with_value(5, 3, 7))print(create_list_with_value(5, 3, 7))
+print(create_list_with_value(1, 0, 5))print(create_list_with_value(1, 0, 5))
+print(create_list_with_value(10, 9, 9))print(create_list_with_value(10, 9, 9))
+print(create_list_with_value(10, 9, 0))print(create_list_with_value(10, 9, 0))
