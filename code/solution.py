05339d03f1a92e73b8df401047dea2c9242959e9
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-15 19:52:58 +0530

    Update solution for Code

--- code/solution.py
+++ code/solution.py
@@ -1,17 +1,23 @@
 """
 Problem: Code
-URL: https://neetcode.io/problems/python-resizable-list-part-2/solution
+URL: https://neetcode.io/problems/python-list-concat/solution
 Language: python
 
 Solution by NeetCode GitHub Pusher
 """
 
+from typing import Listfrom typing import List
+
+
+def combine_elements(arr1: List[int], arr2: List[int]) -> List[int]:def combine_elements(arr1: List[int], arr2: List[int]) -> List[int]:
+    return arr1+arr2    return arr1+arr2
+
 
 
 # do not modify below this line# do not modify below this line
-print(append_elements([1, 2, 3], [4, 5, 6]))print(append_elements([1, 2, 3], [4, 5, 6]))
-print(append_elements([4, 3], [4, 5, 3]))print(append_elements([4, 3], [4, 5, 3]))
+arr1 = [1, 3, 5]arr1 = [1, 3, 5]
+arr2 = [4, 6, 8]arr2 = [4, 6, 8]
 
-print(remove_elements([1, 2, 3, 4, 5], [2, 4, 6]))print(remove_elements([1, 2, 3, 4, 5], [2, 4, 6]))
-print(remove_elements([1, 2, 3, 4, 5], [2, 3, 4, 5, 5]))print(remove_elements([1, 2, 3, 4, 5], [2, 3, 4, 5, 5]))
-print(remove_elements([1, 7, 2, 3, 4, 5], [6, 7, 8, 2]))print(remove_elements([1, 7, 2, 3, 4, 5], [6, 7, 8, 2]))
+print(combine_elements(arr1, arr2))print(combine_elements(arr1, arr2))
+print(arr1)print(arr1)
+print(arr2)print(arr2)
