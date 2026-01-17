f0b3e51c044d343ca0ad8a48a252ce7f556ee419
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-17 20:59:07 +0530

    Add solution for List Comprehension

---
+++ list-comprehension/solution.py
@@ -0,0 +1,21 @@
+"""
+Problem: List Comprehension
+URL: https://neetcode.io/problems/python-list-comprehension/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+from typing import Listfrom typing import List
+
+
+def create_list_of_odds(n: int) -> List[int]:def create_list_of_odds(n: int) -> List[int]:
+        
+
+
+# do not modify below this line# do not modify below this line
+print(create_list_of_odds(1))print(create_list_of_odds(1))
+print(create_list_of_odds(5))print(create_list_of_odds(5))
+print(create_list_of_odds(6))print(create_list_of_odds(6))
+print(create_list_of_odds(10))print(create_list_of_odds(10))
+    print = [i for i in range(1,n+1,2)]    print = [i for i in range(1,n+1,2)]
