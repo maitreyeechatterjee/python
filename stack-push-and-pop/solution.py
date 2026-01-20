018369cc329edfe4bc6cc8738bf9d8d5506116fa
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-20 20:19:17 +0530

    Add solution for Stack Push and Pop

---
+++ stack-push-and-pop/solution.py
@@ -0,0 +1,17 @@
+"""
+Problem: Stack Push and Pop
+URL: https://neetcode.io/problems/python-stack-push-pop/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+from typing import Listfrom typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:def reverse_list(arr: List[int]) -> List[int]:
+    pass    pass
+
+
+# do not modify below this line# do not modify below this line
+print(reverse_list([1, 2, 3]))print(reverse_list([1, 2, 3]))
