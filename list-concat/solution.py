910498be8b70118659c69d861b921196f0365d5a
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-20 20:20:05 +0530

    Add solution for List Concat

---
+++ list-concat/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: List Concat
+URL: https://neetcode.io/problems/python-list-concat/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+def combine_elements(arr1: List[int], arr2: List[int]) -> List[int]:def combine_elements(arr1: List[int], arr2: List[int]) -> List[int]:
+    return arr1+arr2    return arr1+arr2
+
+
+
+# do not modify below this line# do not modify below this line
+arr1 = [1, 3, 5]arr1 = [1, 3, 5]
+arr2 = [4, 6, 8]arr2 = [4, 6, 8]
