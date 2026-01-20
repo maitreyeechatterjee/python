6e513de51857067946d8f7f13763ad3a9cbad474
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-01-20 20:20:31 +0530

    Update solution for List Comprehension

--- list-comprehension/solution.py
+++ list-comprehension/solution.py
@@ -6,12 +6,12 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-from typing import Listfrom typing import List
-
-
 def create_list_of_odds(n: int) -> List[int]:def create_list_of_odds(n: int) -> List[int]:
+    return  [i for i in range(1,n+1,2)]    return  [i for i in range(1,n+1,2)]
         
 
 
-    return = [i for i in range(1,n+1,2)]    return = [i for i in range(1,n+1,2)]
 # do not modify below this line# do not modify below this line
+print(create_list_of_odds(1))print(create_list_of_odds(1))
+print(create_list_of_odds(5))print(create_list_of_odds(5))
+print(create_list_of_odds(6))print(create_list_of_odds(6))
