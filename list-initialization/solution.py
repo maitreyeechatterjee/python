37c7dce17fb5b5e57622591630bc211a8751f4c3
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-02-03 19:32:31 +0530

    Update solution for List Initialization

--- list-initialization/solution.py
+++ list-initialization/solution.py
@@ -6,12 +6,11 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
+from typing import Listfrom typing import List
+
+
 def create_list_with_value(size: int, index: int, value: int) -> List[int]:def create_list_with_value(size: int, index: int, value: int) -> List[int]:
           
       arr = [0] * size      arr = [0] * size
       arr[index] = value      arr[index] = value
       return arr      return arr
-
-
-
-# do not modify below this line# do not modify below this line
