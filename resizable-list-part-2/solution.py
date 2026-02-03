38975db457daedacaf017f1941dd63955fdff903
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-02-03 18:40:31 +0530

    Update solution for Resizable List Part 2

--- resizable-list-part-2/solution.py
+++ resizable-list-part-2/solution.py
@@ -6,12 +6,12 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
+from typing import Listfrom typing import List
+
+
 def append_elements(arr1: List[int], arr2: List[int]) -> List[int]:def append_elements(arr1: List[int], arr2: List[int]) -> List[int]:
     arr1.extend(arr2)    arr1.extend(arr2)
     return arr1    return arr1
     
 
 def remove_elements(arr1: List[int], arr2: List[int]) -> List[int]:def remove_elements(arr1: List[int], arr2: List[int]) -> List[int]:
-    for element in arr2:    for element in arr2:
-        if element in arr1:        if element in arr1:
-            arr1.remove(element)            arr1.remove(element)
