728ec4d3d05efa77fe795a0a59c6c4c1241c003c
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-02-03 23:50:42 +0530

    Update solution for List Clone

--- list-clone/solution.py
+++ list-clone/solution.py
@@ -6,12 +6,11 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
+from typing import Listfrom typing import List
+
+
 def remove_element(arr: List[int], element: int) -> List[int]:def remove_element(arr: List[int], element: int) -> List[int]:
     arr_list = arr.copy()    arr_list = arr.copy()
     arr_list.remove(element)    arr_list.remove(element)
     return arr_list    return arr_list
 
-
-
-# do not modify below this line# do not modify below this line
-arr = [1, 3, 5, 7, 9]arr = [1, 3, 5, 7, 9]
