091a711d18b6bf070b8bbf574be76dfd8e21246d
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-02-06 21:45:39 +0530

    Add solution for Intro to Classes

---
+++ intro-to-classes/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: Intro to Classes
+URL: https://neetcode.io/problems/python-intro-to-classes/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class Pet:class Pet:
+    #constructor    #constructor
+    def __init__(self,name:str,species:str)->None:    def __init__(self,name:str,species:str)->None:
+        self.name=name        self.name=name
+        self.species=species        self.species=species
+
+
+
