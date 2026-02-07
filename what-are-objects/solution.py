8db3fb46a16908e1a4497f3c1118604ba3da613a
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-02-07 21:42:34 +0530

    Add solution for What are Objects?

---
+++ what-are-objects/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: What are Objects?
+URL: https://neetcode.io/problems/python-what-are-objects/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class Pet:class Pet:
+    def __init__(self, name: str, species: str, hunger: int, energy: int):    def __init__(self, name: str, species: str, hunger: int, energy: int):
+        self.name = name        self.name = name
+        self.species = species        self.species = species
+        self.hunger = hunger        self.hunger = hunger
+        self.energy = energy        self.energy = energy
+
+# Don't modify the above code# Don't modify the above code
