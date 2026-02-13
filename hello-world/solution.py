1b142fd7638920e2bac62189a1b5f2f37e2b981d
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-02-13 20:52:11 +0530

    Add solution for Hello, World

---
+++ hello-world/solution.py
@@ -0,0 +1,9 @@
+"""
+Problem: Hello, World
+URL: https://neetcode.io/problems/python-hello-world/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+print("Hello, world!")print("Hello, world!")
