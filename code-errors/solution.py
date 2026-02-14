232a31ee34b5bc7855bb98d0dcf49adea5211398
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-02-14 21:13:02 +0530

    Add solution for Code Errors

---
+++ code-errors/solution.py
@@ -0,0 +1,9 @@
+"""
+Problem: Code Errors
+URL: https://neetcode.io/problems/python-code-errors/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+print("Can someone pls add a closing parenthesis?")print("Can someone pls add a closing parenthesis?")
