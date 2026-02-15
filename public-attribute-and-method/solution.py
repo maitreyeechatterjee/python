0353f30d286bb4898299b783b7c97eb1d053d67a
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-02-15 19:57:23 +0530

    Add solution for Public Attribute and Method

---
+++ public-attribute-and-method/solution.py
@@ -0,0 +1,17 @@
+"""
+Problem: Public Attribute and Method
+URL: https://neetcode.io/problems/python-public-attribute-and-method/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class StoreItem:class StoreItem:
+    def __init__(self,name,price):    def __init__(self,name,price):
+       self.name=name       self.name=name
+       self.price=price       self.price=price
+
+
+chips = StoreItem("Chips", 1.99) # Don't modify this linechips = StoreItem("Chips", 1.99) # Don't modify this line
+
+# TODO: Access the attributes of the chips object and display them# TODO: Access the attributes of the chips object and display them
