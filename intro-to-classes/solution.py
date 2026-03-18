4478881ee0df37e1337f626da8f651ba4eb6d927
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-03-18 20:57:29 +0530

    Update solution for Intro to Classes

--- intro-to-classes/solution.py
+++ intro-to-classes/solution.py
@@ -6,11 +6,12 @@ Language: python
 Solution by NeetCode GitHub Pusher
 """
 
-class Pet:class Pet:
 
     def __init__(self, name, species):    def __init__(self, name, species):
         self.name = name        self.name = name
         self.species = species        self.species = species
 
 pet1 = Pet ("fluffy", "cat") pet1 = Pet ("fluffy", "cat") 
-print ("my pet is a cat named fluffy")print ("my pet is a cat named fluffy")
+print ("My pet is a cat named fluffy")print ("My pet is a cat named fluffy")
+
+
