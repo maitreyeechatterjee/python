16c9978d3836b1d08092f5fd9eaa703495d0a0a1
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-03-31 21:10:13 +0530

    Add solution for Inheritance Basics

---
+++ inheritance-basics/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: Inheritance Basics
+URL: https://neetcode.io/problems/python-inheritance-basics/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+class SmartDevice:class SmartDevice:
+    def __init__(self, name: str):    def __init__(self, name: str):
+        self.name = name        self.name = name
+
+# TODO: Implement the SmartLight class# TODO: Implement the SmartLight class
+
+
+# Don't change the code below# Don't change the code below
