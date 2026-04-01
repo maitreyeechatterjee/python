ba6feea8621f991f88ea3e7e9ef1ce8126919774
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-04-01 21:49:03 +0530

    Add solution for Method Overriding

---
+++ method-overriding/solution.py
@@ -0,0 +1,17 @@
+"""
+Problem: Method Overriding
+URL: https://neetcode.io/problems/python-method-overriding/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+# class Animal:# class Animal:
+#     def __init__(self, name: str):#     def __init__(self, name: str):
+#         self.name = name#         self.name = name
+
+#     def make_sound(self) -> None:#     def make_sound(self) -> None:
+#         print("Animal makes a sound")#         print("Animal makes a sound")
+
+# # TODO: Create a Dog class and a Cat class that inherit from the `Animal` class.# # TODO: Create a Dog class and a Cat class that inherit from the `Animal` class.
+# # TODO: Override the `make_sound` method in the `Dog` class to make the dog bark.# # TODO: Override the `make_sound` method in the `Dog` class to make the dog bark.
