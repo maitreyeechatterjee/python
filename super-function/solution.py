05cd5fd5a8a4d4fa02ba268448cf1e957f219906
Maitreyee Chatterjee <chatterjeemaitreyee10@gmail.com>
2026-04-11 20:38:49 +0530

    Add solution for Super Function

---
+++ super-function/solution.py
@@ -0,0 +1,17 @@
+"""
+Problem: Super Function
+URL: https://neetcode.io/problems/python-super-function/question
+Language: python
+
+Solution by NeetCode GitHub Pusher
+"""
+
+# class SuperHero:# class SuperHero:
+#     def __init__(self, name: str, power: str):#     def __init__(self, name: str, power: str):
+#         self.name = name#         self.name = name
+#         self.power = power#         self.power = power
+        
+#     def attack(self) -> None:#     def attack(self) -> None:
+#         print(f"{self.name} is attacking with {self.power}")#         print(f"{self.name} is attacking with {self.power}")
+
+# # TODO: Create a Avenger class that inherits from the SuperHero class# # TODO: Create a Avenger class that inherits from the SuperHero class
